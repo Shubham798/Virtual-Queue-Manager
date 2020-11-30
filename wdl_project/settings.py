95b35dab352ffea36bb4a3aa8186f0b84fd50b9b
Shubham798 <shubhamchettiar123@gmail.com>
2020-11-30 16:39:43 +0530

    Update settings.py

--- wdl_project/settings.py
+++ wdl_project/settings.py
@@ -127,3 +127,24 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+
+LOGGING = {
+    'version': 1,
+    'handlers': {
+        'console':{
+            'level':'DEBUG',
+            'class':'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django.request': {
+            'handlers':['console'],
+            'propagate': True,
+            'level':'DEBUG',
+        }
+    },
+}
+
+
+django_heroku.settings(locals(), logging=False)
